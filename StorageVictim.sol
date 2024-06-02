3d4241069cc7e89a083d22481401a993933572c6
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-06-03 00:44:04 +0530

    Audited the security vulnerabilities of old solidity contract

---
+++ StorageVictim.sol
@@ -0,0 +1,78 @@
+//SPDX-License-Identifier:MIT
+// added the license identifier.
+
+// pragma solidity ^0.4.23;
+
+// contract StorageVictim {
+//     address owner;
+
+//     struct Storage {
+//         address user;
+//         uint amount;
+//     }
+
+//     mapping(address => Storage) storages;
+
+//     function StorageVictim() public {
+//         owner = msg.sender;
+//     }
+
+//     function store(uint _amount) public {
+//         // uninitialised pointer. str.user points to the storage address 0 which is "owner"
+
+//         Storage str;
+
+//         str.user = msg.sender;
+
+//         str.amount = _amount;
+
+//         storages[msg.sender] = str;
+//     }
+
+//     function getStore() public view returns (address, uint) {
+//         Storage str = storages[msg.sender];
+
+//         return (str.user, str.amount);
+//     }
+
+//     function getOwner() public view returns (address) {
+//         return owner;
+//     }
+// }
+
+pragma solidity 0.8.23; // Version should be latest Solidity Stable Version which is also bug free.
+
+contract StorageVictim {
+    address immutable OWNER; // it should be immutable as once set it will never change. (as immutable should be in all UPPERCASE to show that they are imutable.)
+    // Also immutable state variables cost less gas. (better for optimization and security)
+    struct Storage {
+        address user;
+        uint amount;
+    }
+
+    mapping(address => Storage) storages;
+
+    constructor() {
+        OWNER = msg.sender;
+    }
+
+    function store(uint _amount) public {
+        // uninitialised pointer. str.user points to the storage address 0 which is "owner"
+
+        // in the case of Struture , Array and mapping, we need to explicitely define either it is stored in memory or storage.
+
+        Storage memory str = Storage(msg.sender, _amount); // added memory (its temporarly stored and later will be assigned to other)
+
+        storages[msg.sender] = str;
+    }
+
+    function getStore() public view returns (address, uint) {
+        Storage memory str = storages[msg.sender];
+
+        return (str.user, str.amount);
+    }
+
+    function getOwner() public view returns (address) {
+        return OWNER;
+    }
+}
